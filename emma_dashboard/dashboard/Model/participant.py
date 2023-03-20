ffb63f96c585dd44df2c0eab4b4ba8858a95eb37
Reagan <reagan.kelley@wsu.edu>
2023-03-19 19:10:01 -0700

    Added Health Tracking graphs.

--- emma_dashboard/dashboard/Model/participant.py
+++ emma_dashboard/dashboard/Model/participant.py
@@ -22,12 +22,36 @@ class Participant:
     
     def get_graphs(self):
         self.graphs : dict[VariableGraph] = {}
+        
         self.graphs['PE'] = VariableGraph(
+            chart_id      = 'PE_chart',
             title         = "Physical Exercise",
             graph_type    = "line",
             df            = self.tables, 
-            df_columns    = ['v_TotalPEWeeklyPHEX'], 
+            df_columns    = ['v_TotalPEWeeklyPHEX', 'v_PEGoal'], 
             scope         = 'weekly', 
-            labels        = ['Weekly Hours of Activity'],
-            border_color  = ['red']
+            labels        = ['Weekly Hours of Activity', 'PE Goal'],
+            border_color  = ['#0000FF', '#00FFFF']
+            )
+
+        self.graphs['CE'] = VariableGraph(
+            chart_id      = 'CE_chart',
+            title         = "Cognitive Exercise",
+            graph_type    = "line",
+            df            = self.tables, 
+            df_columns    = ['v_TotalCEWeeklyMEEX', 'v_CEGoal'], 
+            scope         = 'weekly', 
+            labels        = ['Weekly Hours of Activity', 'CE Goal'],
+            border_color  = ['#A52A2A', '#C19A6B']
+            )
+        
+        self.graphs['WE'] = VariableGraph(
+            chart_id      = 'WE_chart',
+            title         = "Well-being Exercise",
+            graph_type    = "line",
+            df            = self.tables, 
+            df_columns    = ['v_TotalWEWeeklyWELLX', 'v_WEGoal'], 
+            scope         = 'weekly', 
+            labels        = ['Weekly Hours of Activity', 'WE Goal'],
+            border_color  = ['#228B22', '#AFE1AF']
             )
