458ff9a769e45171714a750f32064b5470d42aeb
Reagan <reagan.kelley@wsu.edu>
2023-07-25 14:50:29 -0400

    added time ranges to charts.

--- emma_dashboard/dashboard/View/static/js/index.js
+++ emma_dashboard/dashboard/View/static/js/index.js
@@ -1,4 +1,70 @@
 function createChart(chartJSON) {
-    console.log("HELLO THERE")
-    console.log(chartJSON);
+
+    switch(chartJSON['graph_type']) {
+        case 'line':
+            createLineChart(chartJSON);
+            break;
+        default:
+            console.log("Error: Could not load chart of type: " + chartJSON['graph_type'])
+    }
+}
+
+function createLineChart(chartJSON) {
+
+  var ctx = document.getElementById(chartJSON['chart_id']);
+  
+  var min_date_cutoff = new Date(chartJSON['datasets'][0]['data'].slice(-1)[0]['x'])
+  min_date_cutoff.setMonth(min_date_cutoff.getMonth() - 1)
+
+  const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: chartJSON['labels'],
+        datasets: chartJSON['datasets'],
+      },
+      options: {
+       
+        maintainAspectRatio: false,
+        responsive: true,
+
+        scales: {
+            y: { beginAtZero: true},
+            
+            x: {
+              type: 'timeseries',
+              time: {
+                unit: 'day'
+              },
+              min: min_date_cutoff,
+
+              ticks: {
+                stepSize: 7
+              }
+            }
+          },
+
+      plugins: {
+          title: {
+              display: true,
+              text: chartJSON['title'],
+              color: 'black',
+              font: {
+                size: 14,
+                family: 'Courier New'
+              }
+          },
+          legend: {
+              labels: {
+                  // This more specific font property overrides the global property
+                  font: {
+                    size: 12,
+                    family: 'Courier New'
+                  }
+              }
+          }
+      }
+  }
+  });
+
+
 }
